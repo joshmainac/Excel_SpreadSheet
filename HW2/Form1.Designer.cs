5e9fb57b19b9eb965c48e73ea678ea4e8fa6a5e4
josh.mainac <joshua.long@wsu.edu>
2022-09-14 13:56:53 -0700

    add DistinctIntegers()

--- HW2/Form1.Designer.cs
+++ HW2/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblHelloWorld3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblHelloWorld3
+            // 
+            this.lblHelloWorld3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblHelloWorld3.Location = new System.Drawing.Point(0, 0);
+            this.lblHelloWorld3.Multiline = true;
+            this.lblHelloWorld3.Name = "lblHelloWorld3";
+            this.lblHelloWorld3.Size = new System.Drawing.Size(800, 450);
+            this.lblHelloWorld3.TabIndex = 0;
+            this.lblHelloWorld3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHelloWorld3);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox lblHelloWorld3;
     }
 }
 
--- HW2/Form1.cs
+++ HW2/Form1.cs
@@ -15,6 +15,112 @@ namespace HW2
         public Form1()
         {
             InitializeComponent();
+            RunDistinctIntegers();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int DistinctIntegersOne(int[] array)
+        {
+            //int[] distinctArray = array.Distinct().ToArray();
+            //lblHelloWorld.Text = Convert.ToString(distinctArray.Count());
+            //lblHelloWorld2.Text = Convert.ToString(distinctArray.Count());
+            HashSet<int> myhash = new HashSet<int>();
+            int i;
+            for (i = 0; i < array.Length; i++)
+            {
+                myhash.Add(array[i]);
+
+            }
+
+            return myhash.Count;
+        }
+
+        private int DistinctIntegersTwo(int[] array)
+        {
+            //return number of disctinct integers in O(1) space
+            int mynum = array.Length;
+            int dd = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        mynum--;
+                        break;
+
+                    }
+
+                }
+
+            }
+            return mynum;
+        }
+
+        private int DistinctIntegersThree(int[] array)
+        {
+            //sort list
+            Array.Sort(array);
+            int mynum = array.Length;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] == array[i + 1])
+                {
+                    mynum--;
+                }
+            }
+            return mynum;
+
+        }
+
+        private void RunDistinctIntegers() // this is your method
+        {
+            int[] numbers = new int[10000];
+
+            int i = 0;
+            //HashSet<int> myhash = new HashSet<int>();
+            //random number generator
+            Random rand = new Random();
+            //create 10000 rundom numbers
+            for (i = 0; i < 10000; i++)
+            {
+                numbers[i] = rand.Next(0, 20000);
+
+            }
+
+
+            int resOne = DistinctIntegersOne(numbers);
+            int resTwo = DistinctIntegersTwo(numbers);
+            int resThree = DistinctIntegersThree(numbers);
+
+
+
+
+
+            lblHelloWorld3.Text = "1. Hash Set method: " + resOne + " unique numbers" + Environment.NewLine +
+                "Here you will have info about the time complexity of your code" + Environment.NewLine +
+                "You also need info about how you determined it" + Environment.NewLine +
+                "2. O(1) storage method: " + resTwo + " unique numbers" + Environment.NewLine +
+                "3. Sorted method: " + resThree + " unique numbers";
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }

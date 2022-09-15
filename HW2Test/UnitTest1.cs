8874e2df5f8afb9010aa78cb106940732459d7f9
josh.mainac <joshua.long@wsu.edu>
2022-09-14 23:24:17 -0700

    update test

--- HW2Test/UnitTest1.cs
+++ HW2Test/UnitTest1.cs
@@ -1,8 +1,9 @@
 using NUnit.Framework;
+using HW2;
 
 namespace HW2Test
 {
-    public class Tests
+    public class Form1Tests
     {
         [SetUp]
         public void Setup()
@@ -12,7 +13,17 @@ namespace HW2Test
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //Assert.Pass();
+            //test distinctnumbers
+            int[] testArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] testArray2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Form1 myForm = new Form1();
+            
+
+
+
+
+
         }
     }
 }

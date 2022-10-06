db77a8b99113e4f541b3901276350a03bbebd497
josh.mainac <joshua.long@wsu.edu>
2022-10-06 00:01:34 -0700

    add project

---
+++ Spreadsheet_Joshua_Long/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Spreadsheet_Joshua_Long
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.A = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.B = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.D = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.E = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.G = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.A,
+            this.B,
+            this.C,
+            this.D,
+            this.E,
+            this.F,
+            this.G});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // A
+            // 
+            this.A.HeaderText = "A";
+            this.A.MinimumWidth = 6;
+            this.A.Name = "A";
+            this.A.Width = 125;
+            // 
+            // B
+            // 
+            this.B.HeaderText = "B";
+            this.B.MinimumWidth = 6;
+            this.B.Name = "B";
+            this.B.Width = 125;
+            // 
+            // C
+            // 
+            this.C.HeaderText = "C";
+            this.C.MinimumWidth = 6;
+            this.C.Name = "C";
+            this.C.Width = 125;
+            // 
+            // D
+            // 
+            this.D.HeaderText = "D";
+            this.D.MinimumWidth = 6;
+            this.D.Name = "D";
+            this.D.Width = 125;
+            // 
+            // E
+            // 
+            this.E.HeaderText = "E";
+            this.E.MinimumWidth = 6;
+            this.E.Name = "E";
+            this.E.Width = 125;
+            // 
+            // F
+            // 
+            this.F.HeaderText = "F";
+            this.F.MinimumWidth = 6;
+            this.F.Name = "F";
+            this.F.Width = 125;
+            // 
+            // G
+            // 
+            this.G.HeaderText = "G";
+            this.G.MinimumWidth = 6;
+            this.G.Name = "G";
+            this.G.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn A;
+        private System.Windows.Forms.DataGridViewTextBoxColumn B;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn D;
+        private System.Windows.Forms.DataGridViewTextBoxColumn E;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F;
+        private System.Windows.Forms.DataGridViewTextBoxColumn G;
+    }
+}
+

fa99e92b5c2f09d9b3c61b41bb0ed066aad1b79b
josh.mainac <joshua.long@wsu.edu>
2022-10-06 01:09:15 -0700

    add test project

---
+++ Spreadsheet_Joshua_LongTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Spreadsheet_Joshua_LongTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

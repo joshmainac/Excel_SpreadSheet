762d184e2a0fbe612b18d8f560b20000570c4076
josh.mainac <joshua.long@wsu.edu>
2022-10-26 11:20:00 -0700

    setup tests for TDD

---
+++ SpreadsheetTests/UnitTest1.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+using ExpressionTreeEngine;
+
+namespace SpreadsheetTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void TestAddition()
+        {
+            ExpressionTree tree = new ExpressionTree("5+10+10");
+            Assert.That(tree.Evaluate() == 25);
+
+        }
+        [Test]
+        public void TestSubtraction()
+        {
+            ExpressionTree tree = new ExpressionTree("25-1");
+            Assert.That(tree.Evaluate() == 24);
+        }
+        [Test]
+        public void TestDivision()
+        {
+            ExpressionTree tree = new ExpressionTree("24/12");
+            Assert.That(tree.Evaluate() == 2);
+        }
+
+        [Test]
+        public void TestMultiplication()
+        {
+            ExpressionTree tree = new ExpressionTree("2*8");
+            Assert.That(tree.Evaluate() == 16);
+
+        }
+
+    }
+}
